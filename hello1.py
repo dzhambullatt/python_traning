93f817d7ff34f1f77267c4f921c22dd5ef482223
djambulat <jambulatahmadov99@gmail.com>
2021-09-21 16:00:09 +0300

    python stady

--- hello1.py
+++ hello1.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 # person = {'name': 'ivan', 'surname': 'rizvan'}
 
 # d = dict(name='petr', ihvan='amhad')
@@ -76,8 +77,71 @@
 # 	print(key, value)
 
 
-inter = 0
-while inter <= 100:
-	print(inter)
-	inter += 1
+# inter = 0
+# while inter <= 100:
+# 	print(inter)
+# 	inter += 1
 
+# jack = {
+# 	'name': 'jack',
+# 	'car': 'bmw'
+# }
+
+# jhon = {
+# 	'name': 'jhon',
+# 	'car': 'audi'
+# }
+
+# users = [jack, jhon]
+
+# cars = [i.get('car') for i in users]
+
+# print(cars)
+
+
+# name = ['jack', 'jhon', 'oleg', 'ula']
+
+# #new_names = [n for n in name if n.startswith('j')]
+
+# #print(new_names)
+
+# new_name = []
+# for n in name:
+# 	if n.startswith('j'):
+# 		new_name.append(n)
+
+# print(new_name)
+
+def timeit(func):
+	def wrapper():
+		start = datetime.now()
+		result = func()
+		print(datetime.now() - start)
+		return result
+	return wrapper
+
+@timeit
+def one():
+	#start = datetime.now()
+	l = []
+	for i in range(10**4):
+		if i % 2 == 0:
+			l.append(i)
+	#rint(datetime.now() - start)
+	return l
+
+@timeit
+def two():
+	#start = datetime.now()
+	l = [x for x in range(10**4) if x % 2 == 0]
+	#print(datetime.now() - start)
+	return l
+
+
+
+
+l1 = one()
+l2 = two()
+
+# print(l1)
+# print(l2)
